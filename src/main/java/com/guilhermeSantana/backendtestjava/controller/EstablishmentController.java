c122c500b1d7173dd6c9d753e0de93aaa675c608
Guilherme Santana <guiguilsantana@gmail.com>
2024-05-15 15:30:01 -0300

    feat: Created EstablishmentController.java and POST Endpoint web for requisition

---
+++ src/main/java/com/guilhermeSantana/backendtestjava/controller/EstablishmentController.java
@@ -0,0 +1,27 @@
+package com.guilhermeSantana.backendtestjava.controller;
+
+import com.guilhermeSantana.backendtestjava.domain.Establishment;
+import com.guilhermeSantana.backendtestjava.dtos.EstablishmentDto;
+import com.guilhermeSantana.backendtestjava.service.EstablishmentService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/establishments")
+public class EstablishmentController {
+
+    private final EstablishmentService service;
+
+    public EstablishmentController(EstablishmentService service){
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Establishment> CreateEstablishment(@Valid @RequestBody EstablishmentDto data){
+        Establishment newEstablishment = service.createEstablishment(data);
+        return new ResponseEntity<>(newEstablishment, HttpStatus.CREATED);
+    }
+
+}
