c3bc8b6c523eeae31552264fc757d55427ddda3f
Guilherme Santana <guiguilsantana@gmail.com>
2024-05-21 21:14:46 -0300

    feat: created EntryExitService and method register entry and exit

---
+++ src/main/java/com/guilhermeSantana/backendtestjava/service/EntryExitService.java
@@ -0,0 +1,46 @@
+package com.guilhermeSantana.backendtestjava.service;
+
+import com.guilhermeSantana.backendtestjava.domain.EntryExit;
+import com.guilhermeSantana.backendtestjava.domain.Establishment;
+import com.guilhermeSantana.backendtestjava.domain.Vehicle;
+import com.guilhermeSantana.backendtestjava.repositories.EntryExitRepository;
+import com.guilhermeSantana.backendtestjava.repositories.EstablishmentRepository;
+import com.guilhermeSantana.backendtestjava.repositories.VehicleRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Service
+public class EntryExitService {
+
+    private final EntryExitRepository entryExitRepository;
+
+    private final VehicleRepository vehicleRepository;
+
+    private final EstablishmentRepository establishmentRepository;
+
+    public EntryExitService(EntryExitRepository entryExitRepository, VehicleRepository vehicleRepository, EstablishmentRepository establishmentRepository){
+        this.entryExitRepository = entryExitRepository;
+        this.vehicleRepository = vehicleRepository;
+        this.establishmentRepository = establishmentRepository;
+    }
+
+
+    public EntryExit registerEntry(String plate, String cnpj) throws Exception{
+        Vehicle vehicle = this.vehicleRepository.findByplate(plate).orElseThrow(() -> new Exception("Veículo não encontrado"));
+
+        Establishment establishment = this.establishmentRepository.findEstablishmentByCnpj(cnpj).orElseThrow(() -> new Exception("Estabelecimento não encontrado"));
+
+        EntryExit entryExit = new EntryExit();
+        entryExit.setVehicle(vehicle);
+        entryExit.setEstablishment(establishment);
+        entryExit.setEntryDate(LocalDateTime.now());
+        entryExit.setExitDate(LocalDateTime.now());
+
+        entryExitRepository.save(entryExit);
+
+        return entryExit;
+    }
+
+}
